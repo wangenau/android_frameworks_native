d04ae4707771bbc4291be0649669abe2668e3869
zhongjie <zhongjie.shi@intel.com>
2016-03-09 15:05:04 +0800

    Release resources when repeating dlopen-dlcose.
    When the libbinder.so is dlopen and dlclose many
    times, we need to delete the pthread_key and
    munmap the memory mapping.
    
    Otherwise, it will crash as the leakage causes
    the pthread_key or memory mapping surpass their
    limit.
    
    Change-Id: Ice64d4ef685c1f665350286f9e5ddf790beac236
    Signed-off-by: zhongjie <zhongjie.shi@intel.com>

--- libs/binder/IPCThreadState.cpp
+++ libs/binder/IPCThreadState.cpp
@@ -341,6 +341,7 @@ void IPCThreadState::shutdown()
             delete st;
             pthread_setspecific(gTLS, NULL);
         }
+        pthread_key_delete(gTLS);
         gHaveTLS = false;
     }
 }
